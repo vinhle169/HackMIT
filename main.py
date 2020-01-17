20977b882a3f46828dbfeb1d91cae796790054e1
Vinh Le <vinhle184@gmail.com>
2020-01-17 10:55:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ def main():
 	if text==None or len(text)<1:
 		labels=detect_labels_local_file(photo,bucket)
 		try:
-			print('hereeeee')
-			print(labels)
 			for i in labels:
 				query = f"Amazon:{i[0]}"
 				for j in search(query, tld="co.in",num=1,stop=1,pause=1):
@@ -24,11 +22,10 @@ def main():
 					print(j)
 					webbrowser.open_new(j)
 		except:
-			print('oopsie doopsie')
+			print('Not found')
 	else:
 		try:
 			if manywords:
-				print('nani')
 				text = "Amazon:"+' '.join(text[0:combinewords])
 				for j in search(text, tld="co.in",num=1,stop=1,pause=1):
 						if j[0:22]!='https://www.amazon.com':
@@ -36,7 +33,6 @@ def main():
 						print(j)
 						#webbrowser.open_new(j)
 			else:
-				print('nuni')
 				for i in text:
 					query = f"Amazon:{i}"
 					for j in search(query, tld="co.in",num=1,stop=1,pause=1):
@@ -46,7 +42,7 @@ def main():
 						print(j)
 						webbrowser.open_new(j)
 		except:
-			print('ooooopsie doooooopsie')
+			print('Too many words')
 if __name__ == "__main__":
 	main()
 ####doesnt work when there are way too many words 
